654e815985e4c6862ac4525238216ef498e47e0b
Neon <elimbai.z2408@gmail.com>
2026-08-18 21:57:27 +0500

    проверка в C# и тип даных List и Итерация с foreach

---
+++ проверка_try_and_catch.cs
@@ -0,0 +1,65 @@
+﻿using System;
+class Programing
+{
+    static void Main()
+    {
+        Dictionary<string, int> math = new Dictionary<string, int>();
+        math["2+4="] = 6;
+        math["5*8="] = 40;
+        math["20*34="] = 748;
+        math["54*13="] = 702;
+        while (true)
+        {
+            try
+            {
+                Console.Write("Салам как тебя зовут: ");
+                string name = Console.ReadLine();
+                if (string.IsNullOrEmpty(name))
+                {
+                    Console.WriteLine("пусто");
+                    continue;
+                }
+                Console.WriteLine($"тебя зовут {name}");
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("только буквы");
+                continue;
+            }
+
+            try
+            {
+                Console.Write("Сколько тебе лет: ");
+                int age = int.Parse(Console.ReadLine());
+                Console.WriteLine($"тебе {age}");
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("только цифры");
+                continue;
+            }
+            try
+            {
+                foreach (var item in math)
+                {
+                    Console.Write(item.Key);
+                    int user_op = int.Parse(Console.ReadLine());
+                    if (user_op == item.Value)
+                    {
+                        Console.WriteLine("Правильно");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неверно");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка");
+            }
+        }
+    }
+}
